e53d1be84da49a61cad2d1de5b3d2002503fc452
benjaminhlina <benjamin.hlina@gmail.com>
2022-03-18 09:25:25 -0400

    Change method to be more refined by mapping over rec_compare and creating sf object then calculating distance

--- R/shortest_path_example.r
+++ R/shortest_path_example.r
@@ -53,16 +53,20 @@ plot(lake_spd)
 ext <- extent(r)
 
 # create raster and change the resolution to 5 m
+# Depending on the system this can be adjusted to whatever resolution you desire 
+# if in a large system res could be less fine scaled 
 s <- raster(r, res = 5)
 
 # remove and change NA values to fit within the extent
 s <- rasterize(x = r, y = s, field = 1)
 
-# plot raster to make sure it looks apporipriate
+# plot raster to make sure it looks appropriate
 plot(s)
 
 
 # create transition layer with directions being queens space
+# if in larger systems direction could be reduced from queens space to 
+# rook or king, 4 or 8 
 trans <- transition(x = s, transitionFunction = mean, directions = 16)
 
 
@@ -70,14 +74,28 @@ trans <- transition(x = s, transitionFunction = mean, directions = 16)
 # You will need to replace zone with the correct UTM ZONE
 rec_sum <- spTransform(rl_sum_spd, CRS("+proj=utm +zone=15 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")) %>%
   as_tibble() %>%
+  mutate(rec_group = factor(rec_group, 
+                            levels = c("Head",
+                                       "Neck",
+                                       "Body",
+                                       "Arm",
+                                       "Legs"))) %>% 
   rename(lon = coords.x1,
          lat = coords.x2) %>%
   arrange(lon)
 
 
+
+# create a to and from list using rec group names
+from_to <- rec_sum %>%
+  arrange(lon) %>%
+  mutate(from = rec_name,
+         to = rec_name) %>%
+  expand(from, to) %>%
+  mutate(from_to = paste0(from, "-", to))
+
 # create rec lat and long compare tibble -----
 rec_compare <- rec_sum %>%
-  dplyr::select(-rec_name, -rec_group) %>%
   mutate(llon = lon,
          llat = lat,
          lonlat = paste0(lon, ",", lat),
@@ -95,75 +113,32 @@ rec_compare <- rec_sum %>%
 # rec_5 <- rec_compare %>%
 #   filter(id < 5)
 
-# calculate the distance between every receiver -----
-rec_dist <- rec_compare %>%
-  split(.$id) %>%
-  map(possibly(~shortestPath(trans,
-                             c(.$llon, .$llat), c(.$lon, .$lat),
-                             output = "SpatialLines"), NA)) %>%
-  map(possibly(~gLength(.), NA)) %>%
-  map_df(., as_tibble) %>%
-  rownames_to_column("id" ) %>%
-  mutate_if(is.character, function(x) as.numeric(x))  %>%
-  rename(cost_dist = value)
-
-
-
-# create sf object for the caclucating the distance between every rec -----
+# calculate the distance between every receiver and create  sf object -----
 rec_dist_sf <- rec_compare %>%
   split(.$id) %>%
-  map(possibly(~shortestPath(trans,
-                             c(.$llon, .$llat),
-                             c(.$lon, .$lat),
-                             output = "SpatialLines"), NA)) %>%
-  map(possibly(~st_as_sf(., crs = 26918), NA), .id = "id") %>%
+  map(possibly(~ shortestPath(trans,
+                              c(.$llon, .$llat),
+                              c(.$lon, .$lat),
+                              output = "SpatialLines"), NA)) %>%
+  map(possibly(~ st_as_sf(., crs = 32615), NA)) %>% # u will need to replace CRS
   bind_rows() %>%
-  rownames_to_column("id") %>%
-  mutate_if(is.character, function(x) as.numeric(x))
+  mutate(id = c(1:nrow(.)), 
+         cost_dist = as.numeric(st_length(.)))# st_length determines path length
 
-######### start working on joining everything so they line up ---------
-# add rec group to rec summary
-rec_sum <- rec_sum %>%
-  mutate(rec_group = factor(rl_sum_sf$rec_group, 
-                            levels = c("Head",
-                                       "Neck",
-                                       "Body",
-                                       "Arm",
-                                       "Legs")
-                            ))
+# add in the additional columns to know where the linestrings are going to
 
+rec_dist_sf <- rec_dist_sf %>% 
+  mutate(from = from_to$from, 
+         to = from_to$to,
+         from_to = from_to$from_to,
+         from_lon = rec_compare$lon,
+         from_lat = rec_compare$lat,
+         to_lon = rec_compare$llon,
+         to_lat = rec_compare$llat) %>% 
+  dplyr::select(id, from:to_lat, cost_dist, geometry)
 
-# create a to and from list using rec group names
-from_to <- rec_sum %>%
-  arrange(lon) %>%
-  mutate(from = rec_group,
-         to = rec_group) %>%
-  expand(from, to) %>%
-  mutate(from_to = paste0(from, "-", to)) %>%
-  rownames_to_column("id") %>%
-  mutate(id = as.numeric(id))
-
-# rename lon and lat in rec_compare to from and to
-rec_compare <- rec_compare %>%
-  rename(from_lon = lon,
-         from_lat = lat,
-         to_lon = llon,
-         to_lat = llat)
-
-# join to and from to rec_compare
-rec_compare_fr_to <- from_to %>%
-  left_join(rec_compare, by = "id")
-
-
-# join rec_compare_from_to to sf object
-rec_dist_sf <- rec_dist_sf %>%
-  left_join(rec_compare_fr_to, by = "id")
-
-# add distances that were calculated
-rec_dist_sf <-  rec_dist_sf %>%
-  left_join(rec_dist, by = "id")
-
-
+rec_dist_sf
+  
 # covert sf objects from Lat Long to UTM ----
 # You will need to replace zone with the correct UTM ZONE
 lake_utm <- st_transform(lake, crs = 32615)
