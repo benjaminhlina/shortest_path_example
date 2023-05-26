7569d8ae394da5e4f7328b1a27cb794ab1037332
benjamin.hlina <benjamin.hlina@gmail.com>
2023-05-26 08:02:05 -0400

    Formating consistency improved

--- R/pathroutr_shortest_path.r
+++ R/pathroutr_shortest_path.r
@@ -123,7 +123,7 @@ rec_order <- prep_path %>%
       ) %>% 
       st_drop_geometry() %>% 
       rename(from = rec_name) %>% 
-      select(from, lon, lat), ., by = c("lon", "lat"), 
+      select(from, lon, lat), by = c("lon", "lat"), 
     multiple = "all"
   ) %>%  
   left_join(
@@ -134,7 +134,7 @@ rec_order <- prep_path %>%
       st_drop_geometry() %>% 
       rename(to = rec_name,
              llon = lon) %>% 
-      select(to, llon), ., by = c("llon"), 
+      select(to, llon), by = c("llon"), 
     multiple = "all"
   ) %>% 
   mutate(
@@ -206,17 +206,26 @@ ggplot() +
     linewidth = 1) + 
   theme_void()
 
+ggplot() + 
+  geom_sf(data = land_region, fill = "cornsilk3") +
+  geom_sf_label(data = rl_sum_sf, aes(label = rec_name), 
+                size = 4) +
+  geom_sf(
+    data = path_pts %>% # look at just rec 1 to make sure 1 goes to each rec 
+      filter(from == 1), aes(colour = from_to)) + 
+  theme_void()
+
 # ---- use pathroutr to id track points that intersect the shore of the lake ------
 lake_bar_seg <- get_barrier_segments(trkpts = path_pts,
                                      barrier =  land_region) %>% 
   arrange(n_pts)
 
 lake_bar_seg
-
 # ---- create visgraph using land_regeion which is network analysis triangles -----
 # pathroutr will use network analyses of triangles as paths 
 vis <- prt_visgraph(barrier = land_region, 
-                    # buffer = 50 
+                    # aug_points = rl_sum_sf_utm,
+                    buffer = 100
                     # adjust buffer, in this case buffer is 50 m 
                     # away from land
 )
@@ -252,7 +261,6 @@ track_pts_fix <- prt_reroute(path_pts, land_region, vis)
 
 track_pts_fix <- prt_update_points(track_pts_fix, path_pts)
 
-
 # convert in linestrings 
 track_pts_fixed <- track_pts_fix %>% 
   group_by(from_to) %>% 
@@ -269,7 +277,7 @@ track_pts_fixed <- track_pts_fix %>%
 
 
 
-# view one reroute to confimr pathroutr is reouting 
+# view one reroute to confirm pathroutr is rerouting 
 track_pts_fixed %>% 
   filter(from_to == "15-11") %>% 
   ggplot() + 
@@ -295,117 +303,3 @@ ggplot() +
         axis.text = element_text(colour = "black")) + 
   labs(x = "Longitude", 
        y = "Latitude") 
-ggsave(here("Plots", 
-            "big_sissabagama_lake_cost_dist_pathroutr.png"), 
-       height = 8.5, width = 8.5, plot = last_plot())
-
-# ---- simulate dietections 
-
-library(crawl)
-
-rl_sum_sf
-
-
-df <- data.frame(
-  fish_id = "1234",
-  Time = seq.POSIXt(as.POSIXct("2018-05-26 05:06:25"), 
-                    as.POSIXct("2018-06-26 05:06:25"), 
-                    length.out = 10), 
-  rec_name = c(3, 1, 3, 5, 4, 6, 5, 5, 4, 3), 
-  temp = c(5.5, 6.2, 8.2, 8.1, 8.5, 7.8, 7.4, 7.9, 8.5, 7.1), 
-  lat = c(45.79588, 45.7989, 45.79588, 45.79373, 
-          45.7918, 45.79378, 45.79373, 45.79373, 
-          45.7918, 45.79588), 
-  long = c(-91.5236, -91.52844, -91.5236, -91.5191, 
-           -91.52147, -91.51294, -91.5191, -91.5191, 
-           -91.52147, -91.5236)
-  
-  
-) %>% 
-  st_as_sf(., coords = c("long", "lat"), 
-           crs = st_crs(rl_sum_sf)) %>% 
-  st_transform(., crs = 32615)
-df
-unique(harborSeal_sf$DryTime)
-
-
-fixPar <- c(log(250), log(500), log(1500), rep(NA,5), 0)
-fixPar <- c(log(5), log(10), log(50))
-
-
-displayPar(mov.model=~1, 
-           err.model=list(x=~rec_name-1),
-           data = df, 
-           # activity= ~ I(1 - temp),
-           fixPar=fixPar)
-# 
-constr=list(
-  lower=c(rep(log(1500),3), rep(-Inf,2)),
-  upper=rep(Inf,5)
-)
-# 
-# fixPar <- c(log(250), log(500), log(1500), rep(NA,5), 0)
-# 
-m <- crwMLE(
-  mov.model = ~1,
-  err.model = list(x = ~rec_name - 1),
-  # activity = ~I(1-DryTime),
-  data = df,
-  Time.name = "Time",
-  fixPar = fixPar,
-  # theta = c(rep(log(5000),1)),
-  # constr = constr,
-  method = "L-BFGS-B",
-  control = list(maxit = 2000,
-                 trace = 1,
-                 REPORT = 1)
-)
-# 
-# 
-m
-
-unique(harborSeal_sf$Argos_loc_class)
-
-fixPar = c(log(250), log(500), log(1500), rep(NA,5), 0)
-displayPar( mov.model=~1, err.model=list(x=~Argos_loc_class-1),data=harborSeal_sf, 
-            activity=~I(1-DryTime),fixPar=fixPar)
-#>                  ParNames   fixPar thetaIdx
-#> 1 ln tau Argos_loc_class0 5.521461       NA
-#> 2 ln tau Argos_loc_class1 6.214608       NA
-#> 3 ln tau Argos_loc_class2 7.313220       NA
-#> 4 ln tau Argos_loc_class3       NA        1
-#> 5 ln tau Argos_loc_classA       NA        2
-#> 6 ln tau Argos_loc_classB       NA        3
-#> 7    ln sigma (Intercept)       NA        4
-#> 8     ln beta (Intercept)       NA        5
-#> 9                  ln phi 0.000000       NA
-constr=list(
-  lower=c(rep(log(1500),3), rep(-Inf,2)),
-  upper=rep(Inf,5)
-)
-
-set.seed(123)
-fit1 <- crwMLE(
-  mov.model=~1, err.model=list(x=~Argos_loc_class-1), activity=~I(1-DryTime),
-  data=harborSeal_sf,  Time.name="Time", 
-  fixPar=fixPar, theta=c(rep(log(5000),3),log(3*3600), 0),
-  constr=constr, method="L-BFGS-B",
-  control=list(maxit=2000, trace=1, REPORT=1)
-)
-#> Beginning SANN initialization ...
-#> Beginning likelihood optimization ...
-#> iter    1 value 41202.609844
-#> iter    2 value 41056.998394
-#> iter    3 value 40756.499672
-#> iter    4 value 40146.411888
-#> iter    5 value 40066.268387
-#> iter    6 value 40005.050127
-#> iter    7 value 40001.092459
-#> iter    8 value 40001.050325
-#> iter    9 value 40001.048541
-#> iter   10 value 40001.048534
-#> final  value 40001.048534 
-#> converged
-
-pred1 = crwPredict(fit1, predTime = '15 min')
-pred1_sf <- pred1 %>% crw_as_sf("POINT","p")
